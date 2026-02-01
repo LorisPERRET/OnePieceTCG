303c48ca1ec2017583c9b392149d781270f04cb9
LorisPERRET <loris.perret03@gmail.com>
2026-02-01 17:33:10 +0100

    Add Migration

---
+++ prisma/migrations/20260201162059_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "CardData" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "CardData_pkey" PRIMARY KEY ("id")
+);
